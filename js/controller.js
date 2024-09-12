dd1f0da3ad19295303aa7b5c9cb47b81f11b2ada
casoriano03 <casoriano03@gmail.com>
2024-09-12 11:16:15 +0200

    functions working, testing complete

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,30 @@
 function buyCoke() {
-    cokesInStore--;
-    isCokeInDelivery = true;
-    updateView();
+    var coinsPaid = valueFromCoinCounts(coinsInserted)
+    if (coinsPaid < 25) {
+        errorMessage = "Not enough coins inserted"
+        updateView();
+    }
+
+    if (coinsPaid >= 25) {
+        var change = coinsPaid-25
+        var changeInCoins = segregateChangeInCoins(change)
+        checkCokeInStore()
+        addCoinsInMachine(coinsPaid)
+        coinsReturned=changeInCoins
+        valueFromCoinCounts(changeInCoins)
+        reduceCoinsInMachine(changeInCoins)
+        coinsInserted = [0, 0, 0, 0];
+        updateView();
+    }
 }
 
 function insertCoin(value) {
-    coinsInserted[0]++;
+    var index = 0;
+    if (value===1) index=0
+    if (value===5) index=1
+    if (value===10) index=2
+    if (value===20) index=3
+    coinsInserted[index]++;
     updateView();
 }
 
@@ -17,5 +36,86 @@ function returnCoins() {
 
 function takeCoins() {
     coinsReturned = [0, 0, 0, 0];
+    updateView();
+}
+
+function addCoinsInMachine(coinsPaid) {
+    var amountDifference = coinsPaid;
+    while (amountDifference >= 20) {
+        amountDifference-=20
+        coinsInMachine[3]++
+    }
+
+    while (amountDifference >= 10) {
+        amountDifference-=10
+        coinsInMachine[2]++
+    }
+
+    while (amountDifference >= 5) {
+        amountDifference-=5
+        coinsInMachine[1]++
+    }
+
+    while (amountDifference >= 1) {
+        amountDifference-=1
+        coinsInMachine[0]++
+    }
+   
+    return coinsInMachine
+}
+
+function segregateChangeInCoins(change) {
+    var changeDifference = change;
+    var segregatedCoins = [0,0,0,0]
+
+    while (coinsInMachine[3]!==0&&changeDifference >= 20) {
+        changeDifference-=20
+        segregatedCoins[3]++
+    }
+
+    while (coinsInMachine[2]!==0&&changeDifference >= 10) {
+        changeDifference-=10
+        segregatedCoins[2]++
+    }
+
+    while (coinsInMachine[1]!==0&&changeDifference >= 5) {
+        changeDifference-=5
+        segregatedCoins[1]++
+    }
+
+    while (changeDifference >= 1) {
+        changeDifference-=1
+        segregatedCoins[0]++
+    }
+    return segregatedCoins
+}
+
+function checkCokeInStore() {
+    if (cokesInStore === 0) {
+        errorMessage = "Coke Machine is empty"
+    } else {
+        cokesInStore--
+        isCokeInDelivery = true;
+    }
+    updateView();
+}
+
+function reduceCoinsInMachine(changeInCoins) {
+    var remainingOnes = coinsInMachine[0]-changeInCoins[0]
+    var remainingFives = coinsInMachine[1]-changeInCoins[1]
+    var remainingTens = coinsInMachine[2]-changeInCoins[2]
+    var remainingTwenties = coinsInMachine[3]-changeInCoins[3]
+
+    if (remainingOnes === 0) {
+        errorMessage = "One kroner coins is empty"
+    } else if (remainingFives === 0) {
+        errorMessage = "Five kroner coins is empty"
+    } else if (remainingTens === 0) {
+        errorMessage = "Ten kroner coins is empty"
+    } else if (remainingTwenties === 0) {
+        errorMessage = "Twenty kroner coins is empty"
+    }
+
+    coinsInMachine = [remainingOnes, remainingFives, remainingTens, remainingTwenties]
     updateView();
 }
